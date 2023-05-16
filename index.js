4714ac257bc72b86cea98c1f0baf6c788370fda7
IchBinHanz <nilssimons707@gmail.com>
2023-05-16 22:14:00 +0200

    1.*

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 
 const express = require('express')
+
+
+const stockTrendScore = require('./api/stockTrendScore')
+
 const app = express()
-const port = process.env.PORT || 420
+const port = parseInt(process.env.PORT) || 420
 
 app.use(express.static('public'))
 
+
+stockTrendScore.stockTrendScore('LVMH', 10)
 
 app.listen(port, () => {
   console.log(`Tommy running on port ${port}`)
